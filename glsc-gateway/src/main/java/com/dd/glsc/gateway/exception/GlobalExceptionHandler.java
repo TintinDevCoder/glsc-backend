b525b37377f62ce454c11f567fe9ef501d5f3ff1
Administrator <18211882344@163.com>
2025-12-19 15:49:56 +0800

    常量类以及统一报错处理、统一返回

---
+++ glsc-gateway/src/main/java/com/dd/glsc/gateway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.dd.glsc.gateway.exception;
+
+import com.dd.common.common.BaseResponse;
+import com.dd.common.common.BusinessException;
+import com.dd.common.common.ErrorCode;
+import com.dd.common.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 业务相关错误
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 其他错误
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
+
